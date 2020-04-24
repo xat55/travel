87169b832c6643759951f6b6df3783409dddac28
xat55 <xat55@mail.ru>
2020-04-24 11:55:16 +0400

    first commit

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php get_header() ?>
+<div class="content">
+    <h2 class="page-title">Ничего не найдено...</h2>
+    <img src=" <?= get_template_directory_uri() . '/images/404.jpg'; ?> " alt="">
+</div>
+<?php
+    get_sidebar();
+    get_footer();
+?>
